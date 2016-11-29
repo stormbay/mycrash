af99dd132093e740a33351ef1e6694b605087e26
Xin Su <xinsu@qti.qualcomm.com>
2016-11-29 13:17:21 +0800

    update mycrash.sh

--- mycrash.sh
+++ mycrash.sh
@@ -1,19 +1,48 @@
 #!/bin/bash
 
-FILE="load.cmm"
 
-result=""
+BLANK=" "
+COMMA=","
+
+CRASH32="crash-arm"
+CRASH64="crash-arm64"
+
+SYMBOLFILE="vmlinux"
+DUMPFILE="load.cmm"
+
+function usage {
+	echo "Usage: " $(basename $0) "[vmlinux] [load.cmm] [--32bit]"
+}
+
+if [ $# -gt 3 ]; then
+	usage
+	exit
+elif [ $# -ge 2 ]; then
+	SYMBOLFILE=$1
+	DUMPFILE=$2
+fi
+
+if [ $# -eq 3 ] && [ $3 = "--32bit" ]; then
+	CRASH=${CRASH32}
+else
+	CRASH=${CRASH64}
+fi
+
+DUMPLIST=""
 
 while read line
 do
-	tmp=`echo $line | grep "d.load.binary" | grep "DDRCS" | sed -e 's/\ //g;s/d.load.binary//g;s/\/noclear//g;s/BIN/BIN@/g'`
-	if [ -z "$tmp" ]; then
+	TMP=`echo $line | grep "d.load.binary" | grep "DDRCS" | sed -e 's/\ //g;s/d.load.binary//g;s/\/noclear//g;s/BIN/BIN@/g'`
+	if [ -z "${TMP}" ]; then
 		continue
 	fi
-	result=$result$tmp
-done < $FILE
+	DUMPLIST=${DUMPLIST}${TMP}
+done < ${DUMPFILE}
 
-result=`echo $result | sed 's/[0-9]DDRC/;DDRC/g'`
+DUMPLIST=`echo ${DUMPLIST} | sed 's/\(.\)DDRC/\1,DDRC/g'`
 
-echo $result
+RUNCMD=${CRASH}${BLANK}${SYMBOLFILE}${BLANK}${DUMPLIST}
+
+echo ${RUNCMD}
+eval ${RUNCMD}
 
